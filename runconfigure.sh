4e62eeab87535230621f39aebbacae510b67940b
Chris Wolf <cw10025@gmail.com>
2011-01-25 15:38:29 -0500

    Add 'configure' driver script

---
+++ runconfigure.sh
@@ -0,0 +1,11 @@
+# do not run from top level - run from "build" dir:
+# i.e., perform these steps:
+# $ mkdir ./build
+# $ cp runconfigure.sh ./build
+# $ cd ./build
+# $ ./runconfigure.sh
+#
+../configure --enable-load-extension --enable-debug --enable-stored-procedures
+#../configure --enable-load-extension --enable-debug 
+#../configure --enable-load-extension --enable-debug CFLAGS=-DSQLITE_ENABLE_STOREDPROCS
+#../configure --enable-load-extension --disable-amalgamation
